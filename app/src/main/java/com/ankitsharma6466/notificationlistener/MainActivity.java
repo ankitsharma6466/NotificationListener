59c0ef13423513a0bca98eff2fb9fa63948bf46e
Ankit Sharma <ankitsharma6466@gmail.com>
2016-02-03 21:10:15 +0530

    listing notifications on screen

--- app/src/main/java/com/ankitsharma6466/notificationlistener/MainActivity.java
+++ app/src/main/java/com/ankitsharma6466/notificationlistener/MainActivity.java
@@ -7,42 +7,56 @@ import android.content.IntentFilter;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout itemParent;
+    BroadcastReceiver notificationReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        itemParent = (LinearLayout) findViewById(R.id.item_parent);
+        itemParent.removeAllViews();
+
+        notificationReceiver = new BroadcastReceiver() {
+
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                String packageName = intent.getStringExtra(Constants.KEY_PACKAGE);
+                String tickerText = intent.getStringExtra(Constants.KEY_TICKER);
+                String title = intent.getStringExtra(Constants.KEY_TICKER);
+                String body = intent.getStringExtra(Constants.KEY_BODY);
+
+                Log.d("Received", packageName + " --- " + tickerText + " --- " + title + " --- " + body);
+
+                View itemView = getLayoutInflater().inflate(R.layout.item_notification, null);
+                TextView titleView = (TextView) itemView.findViewById(R.id.item_title);
+                TextView bodyView = (TextView) itemView.findViewById(R.id.item_body);
+                TextView packageView = (TextView) itemView.findViewById(R.id.item_package);
+
+                titleView.setText(title);
+                bodyView.setText(body);
+                packageView.setText(packageName);
+
+                itemParent.addView(itemView);
+            }
+        };
+
         LocalBroadcastManager.getInstance(this)
-                .registerReceiver(onNotice, new IntentFilter(Constants.KEY_DATA));
+                .registerReceiver(notificationReceiver, new IntentFilter(Constants.KEY_DATA));
     }
 
-    private BroadcastReceiver onNotice = new BroadcastReceiver() {
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-
-            String packageName = intent.getStringExtra(Constants.KEY_PACKAGE);
-            String tickerText = intent.getStringExtra(Constants.KEY_TICKER);
-            String title = intent.getStringExtra(Constants.KEY_TICKER);
-            String body = intent.getStringExtra(Constants.KEY_BODY);
-
-
-//            TableRow tr = new TableRow(getApplicationContext());
-//            tr.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
-//            TextView textview = new TextView(getApplicationContext());
-//            textview.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1.0f));
-//            textview.setTextSize(20);
-//            textview.setTextColor(Color.parseColor("#0B0719"));
-//            textview.setText(Html.fromHtml(pack + "<br><b>" + title + " : </b>" + text));
-//            tr.addView(textview);
-//            tab.addView(tr);
-
-
-        }
-    };
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(notificationReceiver);
+    }
 }
